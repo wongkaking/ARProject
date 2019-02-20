af39b0d3772ee7b9fba08e2191a03446818f056b
wongkaking <417895394@qq.com>
2019-02-21 00:04:12 +0800

    kary
    Can be very simple reality of the current position. No UI and no design

---
+++ Assets/Script/GetGPS.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetGPS : MonoBehaviour {
+	string GetGps= "";
+	public Text ShowGPS;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (StartGPS ());
+		GetGps = "N:" + Input.location.lastData.latitude + "\r\nE:" + Input.location.lastData.longitude;
+		GetGps = GetGps + "\r\nTime" + Input.location.lastData.timestamp;
+		ShowGPS.text = GetGps;
+		Debug.Log (GetGps);
+	}
+
+	public void UpdateGPS () {
+		StartCoroutine (StartGPS ());
+		GetGps = "N:" + Input.location.lastData.latitude + "\r\nE:" + Input.location.lastData.longitude;
+		GetGps = GetGps + "\r\nTime" + Input.location.lastData.timestamp;
+		ShowGPS.text = GetGps;
+		Debug.Log (GetGps);
+	}
+
+	void Stop () {
+		Input.location.Stop ();
+	}
+
+	IEnumerator StartGPS () {
+		if (!Input.location.isEnabledByUser) {
+			GetGps = "isEnableByUser value is " + Input.location.isEnabledByUser.ToString() + "Please turn on the GPS";
+			yield return false;
+		}
+		Input.location.Start (10.0f, 10.0f);
+
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait> 0) {
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+		if ( maxWait < 1) {
+			GetGps = "Init GPS service time out";
+			yield return false;
+		}
+		if ( Input.location.status == LocationServiceStatus.Failed) {
+			GetGps = "Unable to determin device location";
+			yield return false;
+		} else {
+			GetGps = "N:" + Input.location.lastData.latitude + "\r\nE:" + Input.location.lastData.longitude;
+			GetGps = GetGps + "\r\nTime:" + Input.location.lastData.timestamp;
+			yield return new WaitForSeconds(100);
+		}
+	}
+}
